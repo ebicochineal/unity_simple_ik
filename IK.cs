51fae8246fd929feb385f8737ed1aa613d3c7587
ebicochineal <yz94588@gmail.com>
2020-09-05 10:04:46 +0900

    fix

--- IK.cs
+++ IK.cs
@@ -6,7 +6,6 @@ public class IK : MonoBehaviour {
     public Transform target;
     [Range(1, 1024)] public int iteration = 64;
     [Range(1, 32)] public int chain = 1;
-    
     List<Quaternion> rotations = new List<Quaternion>();
     
     void Start () {
@@ -24,6 +23,7 @@ public class IK : MonoBehaviour {
             t = t.parent;
             t.localRotation = this.rotations[i];
         }
+        float n = (1f / this.chain) * 0.25f;
         
         for (int i = 0; i < this.iteration; ++i) {
             t = this.transform;
@@ -33,7 +33,7 @@ public class IK : MonoBehaviour {
                 Vector3 b = this.transform.position - t.position;
                 // a.Normalize();
                 // b.Normalize();
-                float p = 0.1f * (j+1);
+                float p = n * (j+1);
                 t.localRotation *= Quaternion.AngleAxis(-Vector3.Angle(a, b) * p, t.InverseTransformDirection(Vector3.Cross(a, b)));
             }
         }
